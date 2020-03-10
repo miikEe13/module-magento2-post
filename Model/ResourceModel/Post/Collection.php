bdfe4c009850c026dd56b80549e5d3417a97f9b5
miikEe13 <masramirez12@gmail.com>
2020-03-10 15:34:50 -0600

    collection

---
+++ Model/ResourceModel/Post/Collection.php
@@ -0,0 +1,20 @@
+<?php
+namespace RoiUp\Post\Model\ResourceModel\Post;
+
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+	protected $_idFieldName = 'post_id';
+	protected $_eventPrefix = 'roiup_post_collection';
+	protected $_eventObject = 'post_collection';
+
+	/**
+	 * Define resource model
+	 *
+	 * @return void
+	 */
+	protected function _construct()
+	{
+		$this->_init('RoiUp\Post\Model\Post', 'RoiUp\Post\Model\ResourceModel\Post');
+	}
+
+}
